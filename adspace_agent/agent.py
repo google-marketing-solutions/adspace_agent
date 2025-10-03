dd54be0b0b8968a08c61749489b7531363bdd948
Tony Coconate <coconate@google.com>
2025-10-03 14:24:56 -0500

    feat: adds cm360, sa360, dv360, and more as tools
    Change-Id: I5d7262595c6ba79b0fe337af204b4aee44872cb9

--- adspace_agent/agent.py
+++ adspace_agent/agent.py
@@ -14,41 +14,119 @@
 """The AdSpace Agent main application."""
 
 import os
-from typing import cast
 
 from google.adk.agents import Agent
-from google.adk.tools.bigquery import BigQueryCredentialsConfig
-from google.adk.tools.bigquery import BigQueryToolset
-from google.adk.tools.bigquery.config import BigQueryToolConfig
-from google.adk.tools.bigquery.config import WriteMode
+from google.adk.tools.google_api_tool import BigQueryToolset
+from google.adk.tools.google_api_tool import GoogleApiToolset
 from google.adk.tools.google_api_tool import YoutubeToolset
-import google.auth
-from google.auth.credentials import Credentials
 
-from .tools.google_ads import GoogleAdsToolset
+from adspace_agent.tools.google_ads import GoogleAdsToolset
+from adspace_agent.tools.utilities import UtilitiesToolset
 
-tool_config = BigQueryToolConfig(write_mode=WriteMode.BLOCKED)
+bid_manager_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="doubleclickbidmanager",
+    api_version="v2",
+)
 
-credentials, project_id = google.auth.default()
+bigquery_toolset = BigQueryToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+)
 
-credentials_config = BigQueryCredentialsConfig(
-    credentials=cast(Credentials, credentials)
+campaign_manager_360_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="dfareporting",
+    api_version="v5",
+)
+
+display_video_360_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="displayvideo",
+    api_version="v4",
+)
+
+drive_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="drive",
+    api_version="v3",
+    tool_filter=[
+        "drive_changes_list",
+        "drive_comments_list",
+        "drive_drives_list",
+        "drive_drives_get",
+        "drive_files_list",
+        "drive_files_get",
+        "drive_files_download",
+        "drive_files_export",
+        "drive_permissions_list",
+        "drive_teamdrives_list",
+        "drive_teamdrives_get",
+    ],
+)
+
+search_ads_360_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="searchads360",
+    api_version="v0",
+)
+
+storage_toolset = GoogleApiToolset(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    api_name="storage",
+    api_version="v1",
+    tool_filter=[
+        "storage_buckets_get",
+        "storage_buckets_list",
+        "storage_folders_get",
+        "storage_folders_list",
+        "storage_managed_folders_list",
+        "storage_object_access_controls_get",
+        "storage_object_access_controls_list",
+        "storage_objects_copy",
+        "storage_objects_get",
+        "storage_objects_list",
+    ],
 )
 
 youtube_toolset = YoutubeToolset(
     client_id=os.environ["CLIENT_ID"],
     client_secret=os.environ["CLIENT_SECRET"],
+    tool_filter=[
+        "youtube_activities_list",
+        "youtube_captions_list",
+        "youtube_captions_download",
+        "youtube_channels_list",
+        "youtube_comment_threads_list",
+        "youtube_comments_list",
+        "youtube_live_broadcasts_list",
+        "youtube_live_chat_messages_list",
+        "youtube_live_streams_list",
+        "youtube_members_list",
+        "youtube_memberships_levels_list",
+        "youtube_playlist_images_list",
+        "youtube_playlist_items_list",
+        "youtube_playlist_items_insert",
+        "youtube_playlists_list",
+        "youtube_search_list",
+        "youtube_subscriptions_list",
+        "youtube_super_chat_events_list",
+        "youtube_video_categories_list",
+        "youtube_videos_list",
+        "youtube_videos_get_rating",
+        "youtube_youtube_v3_live_chat_messages_stream",
+    ],
 )
-
-bigquery_toolset = BigQueryToolset(
-    credentials_config=credentials_config, bigquery_tool_config=tool_config
-)
-
-google_ads_toolset = GoogleAdsToolset()
 
 root_agent = Agent(
     name="adspace_agent",
-    model="gemini-2.5-flash",
+    model="gemini-2.5-pro",
     description=(
         "AdSpace Agent is designed to provide a standardized way to integrate "
         + "an LLM with Google Ads, YouTube, and Google Cloud to form a more "
@@ -59,9 +137,15 @@ root_agent = Agent(
         + "creatives, and campaigns."
     ),
     tools=[
-        youtube_toolset,
+        bid_manager_toolset,
         bigquery_toolset,
-        google_ads_toolset,
-        # google_search,
+        campaign_manager_360_toolset,
+        display_video_360_toolset,
+        drive_toolset,
+        search_ads_360_toolset,
+        storage_toolset,
+        youtube_toolset,
+        GoogleAdsToolset(),
+        UtilitiesToolset(),
     ],
 )
